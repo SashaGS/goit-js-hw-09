d853e0289d22547b4e67f01bbafa43666fdbb7d1
SashaGS <151622593+SashaGS@users.noreply.github.com>
2026-06-01 03:18:07 +0000

    Deploying to gh-pages from @ SashaGS/goit-js-hw-09@029fe56f8cb395aaff229a595f37a73ced09acee 🚀

---
+++ 1-gallary.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-Dy2q2e6o.js";document.querySelector("ul.gallery");
+//# sourceMappingURL=1-gallary.js.map
