a0a5f6a82e224087fb6773e2de5fc9cf71e9ade6
SashaGS <Fqva@ukr.net>
2026-06-02 15:37:22 +0300

    'update'

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,24 +6,41 @@ let formData = {
     message: "",
 }
 
-const form = document.getElementById('feedback-form');
+// const form = document.getElementById('feedback-form');
+const form =document.querySelector('.feedback-form')
 // Додаємо обробник події submit
 form.addEventListener('input', (e)=> {
-    e.preventDefault(); // блокує стандартну відправку
     formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     // console.log( JSON.stringify(formData));
-    // e.e
 });
-// console.log(form);
+
 
 // Обробка submit
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(formData);
+    // console.log(formData);
+    if (formData.email==="" || formData.message ==="") {
+        alert("Fill please all fields");
+    } else {
+        console.log(formData);
+    }
     // Очищення після відправки
     localStorage.removeItem(STORAGE_KEY);
     form.reset();
-    formData = { email: "", message: "" };
-    console.log(formData);
+    formData.email   = "";
+    formData.message = "";
+});
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const JSONdata = localStorage.getItem(STORAGE_KEY);
+    const mdata    = JSON.parse(JSONdata);
+    // console.log(mdata);
+    if (mdata.email!=="" || mdata.message!=="") {
+       formData.email   = mdata.email;
+       formData.message = mdata.message;       
+       form.elements['email'].value   = mdata.email;
+       form.elements['message'].value = mdata.message;
+    }
 });
